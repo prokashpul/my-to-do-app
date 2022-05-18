6e65dd9a87f506373023cc4b9dae9736dea3bb85
prokash <pprokash102@gmail.com>
2022-05-19 01:00:20 +0600

    todo apps setup

---
+++ src/Components/Pages/Home/Todo/Todo.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+const Todo = () => {
+  const [totoData, setTodoData] = useState([]);
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const description = e.target.dis.value;
+    setTodoData({ title, description });
+    e.target.title.value = "";
+    e.target.dis.value = "";
+  };
+  console.log(totoData);
+  return (
+    <div className="hero min-h-screen bg-base-200 shadow-2xl">
+      <div className="hero-content text-center">
+        <div className="card md:w-[50vw] min-h-[100vh] bg-base-100 shadow-xl">
+          <div className="card-body ">
+            <h2 className="card-title mb-5 justify-center">Add ToDo !!</h2>
+            <form onSubmit={handelSubmit}>
+              <input
+                type="text"
+                name="title"
+                placeholder="Title"
+                className="input input-bordered input-primary w-full mb-5"
+              />
+              <textarea
+                name="description"
+                className="textarea textarea-primary w-full mb-5"
+                placeholder="Description"
+              ></textarea>
+              <button type="submit" className="btn btn-primary">
+                ADD TODO
+              </button>
+            </form>
+
+            <div class="card bg-base-100 shadow-xl">
+              <div class="card-body">
+                <h2>title</h2>
+                <p>add</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
---
+++ src/Components/Sheard/Header/Header.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = ({ children }) => {
+  return (
+    <div className="drawer">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col">
+        <div className="w-full navbar bg-base-300">
+          <div className="flex-none lg:hidden">
+            <label for="my-drawer-3" className="btn btn-square btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-6 h-6 stroke-current"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+          </div>
+          <div className="flex-1 px-2 mx-2">ToDo</div>
+          <div className="flex-none hidden lg:block">
+            <ul className="menu menu-horizontal">
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {children}
+      </div>
+      <div className="drawer-side">
+        <label for="my-drawer-3" className="drawer-overlay"></label>
+        <ul className="menu p-4 overflow-y-auto w-80 bg-base-100">
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
